0b22884ae5e66292aa7e20982d783f95738963e2
DominiekMeers <70231002+DominiekMeers@users.noreply.github.com>
2020-09-17 12:42:18 -0400

    Return a json object now from a search query
    modified the output to expect json format on return

--- LAMPAPI/ContactSearch.php
+++ LAMPAPI/ContactSearch.php
@@ -12,23 +12,46 @@
 	//goes through database to search for contacts with matching criteria in table
 	$result = $conn->query("SELECT * FROM ContactTable WHERE FirstName LIKE '%" . "$name" . "%' OR LastName LIKE '%" . "$name" . "%'");
 	
-	echo ("SELECT * FROM ContactTable WHERE FirstName LIKE '%{$name}%' OR LastName LIKE '%{$name}%'"."<BR>");
+	//echo ("SELECT * FROM ContactTable WHERE FirstName LIKE '%{$name}%' OR LastName LIKE '%{$name}%'"."<BR>");
 	
 	$searchCount = $result->num_rows;
 	
+	$retValue = '{"userName":"' . $userName . '","contacts":';
+	$retValue .= '[';
 	while ($searchCount > 0)
 	{
 	    $row = mysqli_fetch_array($result);
-	    echo ($row['ContactID'] . " " . $row['FirstName'] . " " . $row['LastName']);
-        echo "<br>";
+	    //echo ($row['ContactID'] . " " . $row['FirstName'] . " " . $row['LastName']);
+        //echo "<br>";
+        $retValue .= '{"contactId":' . $row['ContactID'] . ',"firstName":"' . $row['FirstName'] . '","lastName":"' . $row['LastName'] . '"';
         if ($searchCount != 1)
         {
+            $retValue .= ' },';
             //$searchResults .= ",";
+        }
+        else
+        {
+            $retValue .= ' }';
         }
         $searchCount--;
 	}
-	
 	$result->free_result();
     mysqli_close($conn);
-    ?>
+    
+	$retValue .= ']';
+	$retValue .= '}';
+	sendResultInfoAsJson( $retValue );
+	
+	
+    
+    function getRequestInfo()
+    {
+        return json_decode(file_get_contents('php://input'), true);
+    }
+
+    function sendResultInfoAsJson( $obj )
+	{
+		header('Content-type: application/json');
+		echo $obj;
+	}
 ?>

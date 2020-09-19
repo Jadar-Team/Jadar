d0e0ef317c91ab91877bc3d47f88f5d9bbab958e
Richard B Gullo <gullorichardb@gmail.com>
2020-09-19 15:46:21 -0400

    Pushing Table
    Here you go!

---
+++ js/sandbox2.js
@@ -0,0 +1,83 @@
+
+// Mock JSON data
+var myArray = [
+    {'name':"Michael",'age':30, 'birthdate':'11/10/1989'},
+    {'name':"Mila",'age':32, 'birthdate':'10/1/1989'},
+    {'name':"Paul",'age':29, 'birthdate':'10/14/1940'},
+    {'name':"James Porche", 'age':28, 'birthdate':'6/6/1992'}
+];
+
+// On keyup, we run this function
+$('.form-control').on('keyup',function(){
+    var value = $(this).val();
+    console.log('Value:', value);
+
+    var data = searchTable(value, myArray);
+    buildTable(data);
+})
+
+
+function searchTable(value, data )
+{
+    var filteredData = [];
+
+    for(var i = 0; i < data.length; i++)
+    {
+        value = value.toLowerCase();
+        var name = data[i].name.toLowerCase();
+
+        if(name.includes(value))
+        {
+            filteredData.push(data[i]);
+
+        }
+    }
+
+    return filteredData;
+}
+
+
+$('th').on('click',function(){
+    var column = $(this).data('column');
+    var order = $(this).data('order');
+    var text = $(this).html();
+    text = text.substring(0,text.length-1);
+
+    console.log("Column was clicked", column, order);
+
+    if(order == 'desc')
+    {
+        $(this).data('order','asc'); 
+        myArray = myArray.sort((a,b) => a[column] > b[column] ? 1 : -1);
+        text += '&#9650';
+
+    }
+    else
+    {
+        $(this).data('order','desc');
+        myArray = myArray.sort((a,b) => a[column] < b[column] ? 1 : -1);
+        text += '&#9660';
+    }
+
+    $(this).html(text);
+    buildTable(myArray);
+});
+
+// Function that populates table with json data
+function buildTable(data)
+{
+    var table = document.getElementById('myTable');
+    table.innerHTML= "";
+
+    for(let i = 0; i < data.length; i++)
+    {
+        let row = `<tr>
+                        <td>${data[i].name}</td>
+                        <td>${data[i].age}</td>
+                        <td>${data[i].birthdate}</td>    
+                    </tr>`
+        table.innerHTML += row;
+    }
+}
+
+buildTable(myArray);

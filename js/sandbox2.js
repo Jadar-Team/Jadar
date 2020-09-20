1712374adeadde7224a17136aae85a782868fcc7
Richard B Gullo <gullorichardb@gmail.com>
2020-09-20 03:28:13 -0400

    Updated all sort buttons
    Sort buttons updated

--- js/sandbox2.js
+++ js/sandbox2.js
@@ -1,18 +1,17 @@
 
 // Mock JSON data
 var myArray = [
-    {'fname':"Michael",'lname':"Gullo",'address':"123 street",'phone':"352-87-9780",'email':'knights.edu'},
-    {'fname':"Mila",'lname':"Gullo",'address':"123 street",'phone':"352-87-9780",'email':'knights.edu'},
-    {'fname':"Paul",'lname':"Gullo",'address':"123 street",'phone':"352-87-9780",'email':'knights.edu'},
-    {'fname':"James",'lname':"Gullo",'address':"123 street",'phone':"352-87-9780", 'email': 'knights.edu'} 
+    {'fname':"Michael",'lname':"Oswald",'address':"123 street",'phone':"352-87-9780",'email':'knights.edu', 'date': new Date()},
+    {'fname':"Mila",'lname':"Potter",'address':"123 street",'phone':"352-87-9780",'email':'knights.edu', 'date' : new Date()},
+    {'fname':"Paul",'lname':"Stark",'address':"123 street",'phone':"352-87-9780",'email':'knights.edu', 'date' : new Date()},
+    {'fname':"James",'lname':"Scott",'address':"123 street",'phone':"352-87-9780", 'email': 'knights.edu', 'date' : new Date()} 
 ];
 
-const keys = Object.keys(myArray[0]);
+// Sort JSON data
+myArray = myArray.sort((a,b) => a.fname > b.fname ? 1 : -1);
 
-for(let i = 0; i< keys.length; i++)
-{
-    console.log(keys[i]);
-}
+// Build Table
+buildTable(myArray);
 
 // On keyup, we run this function
 $('#search-bar').on('keyup',function(){
@@ -50,8 +49,6 @@ function buildTable(data)
     var table = document.getElementById('myTable');
     table.innerHTML= "";
 
-    data = data.sort((a,b) => a.name > b.name ? 1 : -1);
-
     for(let i = 0; i < data.length; i++)
     {
         let row = `<tr>
@@ -61,9 +58,26 @@ function buildTable(data)
     }
 }
 
-buildTable(myArray);
+// Toggle between sorting in ascending order first name and last name
+$("#firstLastName").click(function()
+{
+    var order = $(this).data('order');
 
+    if(order == 'first')
+    {
+        $(this).data('order','last')
+        myArray = myArray.sort((a,b) => a.lname > b.lname ? 1 : -1);
+    }
+    else
+    {
+        $(this).data('order','first')
+        myArray = myArray.sort((a,b) => a.fname > b.fname ? 1 : -1);
+    }
 
+    buildTable(myArray);
+})
+
+// addContact button- clears out form fields
 $("#addContact").click(function()
 {
      // Grabs input from each form field
@@ -73,10 +87,24 @@ $("#addContact").click(function()
      })
 })
 
-// Adding a contact into the table
+// recentlyAdded button - sorts users by recently added
+$("#recentlyAdded").click(function()
+{
+    console.log("This is firing at least");
+    myArray = myArray.sort((a,b) => { 
+                                let dateA = new Date(a.date);
+                                let dateB = new Date(b.date);
+                                return dateB - dateA;
+                        });
+
+    buildTable(myArray);
+    console.log(myArray);
+})
+
+// confirm button - modal button that adds user to database
 $("#confirm-add").click(function()
 {
-            const keys = ['fname','lname','address','phone','email'];
+            const keys = ['fname','lname','address','phone','email','date'];
   
             let tempObject = {};
 
@@ -86,11 +114,68 @@ $("#confirm-add").click(function()
                 tempObject[keys[index]] = $(this).val(); 
             })
 
+            // Add date
+            tempObject['date'] = new Date();
+
             // Will eventually be a server call here
             myArray.push(tempObject);
 
-            let markup = "<tr><td>" + tempObject.fname + " " + tempObject.lname + "</td></tr>";
-
-            $("table tbody").append(markup);
+            buildTable(myArray);
 });
 
+// show/hide button - This will Show and hide the table
+$("#showHide").click(function()
+{
+    let order = $(this).data('order');
+
+    if(order == 'show')
+    {
+        $("#myTable").hide();
+        $(this).data('order','hide');
+    }
+    else
+    {
+        $("#myTable").show();
+        $(this).data('order','show');
+    }
+    
+})
+
+
+
+// Am/Pm conversion function
+function displayAmPm(users)
+{
+    for(let i = 0; i < users.length; i++)
+    {
+        var hours = users[i].date.getHours();
+        var minutes = users[i].date.getMinutes();
+        var seconds = users[i].date.getSeconds();
+
+        var ap = "AM";
+        
+        // Hours
+        if(hours > 12)
+        {
+            ap = "PM";
+            hours -= 12;
+        }
+
+        // Minutes
+        if(minutes < 10)
+        {
+            hours = "0" + hours;
+        }
+        
+        // Seconds
+        if(seconds < 10)
+        {
+            minutes = "0" + seconds;
+        }
+
+        let formattedTime = hours + ":" + minutes + ":" + seconds + " " + ap;
+
+        console.log(formattedTime);
+    }
+
+}

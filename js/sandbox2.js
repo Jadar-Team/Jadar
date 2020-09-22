d7c3e1564f333242124aad5e0e6854687c7e994a
aileen <akendrap@gmail.com>
2020-09-22 15:29:24 -0400

    included home icon in info

--- js/sandbox2.js
+++ js/sandbox2.js
@@ -1,7 +1,14 @@
 
 // Mock JSON data
 var myArray = [
-    {'fname':"Michael",'lname':"Oswald",'address':"123 street",'phone':"352-87-9780",'email':'knights.edu', 'date': new Date()},
+    {
+      'fname':"Michael",
+      'lname':"Oswald",
+      'address':"123 street",
+      'phone':"352-87-9780",
+      'email':'knights.edu',
+      'date': new Date()
+    },
     {'fname':"Mila",'lname':"Potter",'address':"123 street",'phone':"352-87-9780",'email':'knights.edu', 'date' : new Date()},
     {'fname':"Paul",'lname':"Stark",'address':"123 street",'phone':"352-87-9780",'email':'knights.edu', 'date' : new Date()},
     {'fname':"James",'lname':"Scott",'address':"123 street",'phone':"352-87-9780", 'email': 'knights.edu', 'date' : new Date()}
@@ -62,6 +69,10 @@ function buildTable(data)
                           </div>
                           <div id="collapseInfo" class="collapse hide" aria-labelledby="contactName" data-parent="#showHide">
                           <div class="card-body">
+                          <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-house-door-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                          <path d="M6.5 10.995V14.5a.5.5 0 0 1-.5.5H2a.5.5 0 0 1-.5-.5v-7a.5.5 0 0 1 .146-.354l6-6a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 .146.354v7a.5.5 0 0 1-.5.5h-4a.5.5 0 0 1-.5-.5V11c0-.25-.25-.5-.5-.5H7c-.25 0-.5.25-.5.495z"/>
+                          <path fill-rule="evenodd" d="M13 2.5V6l-2-2V2.5a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5z"/>
+                        </svg>
                               ${data[i].address + " | " + data[i].phone + " | " + data[i].email}
                             </div>
                             </div>
@@ -107,7 +118,6 @@ function buildTable(data)
           );
     }
 }
-
 
 
 // Toggle between sorting in ascending order first name and last name
@@ -209,6 +219,7 @@ $("#showHide").click(function()
     }
 
 })
+
 
 // Am/Pm conversion function
 function displayAmPm(users)

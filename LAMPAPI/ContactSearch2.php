d3263696060831b5f503a9c3b690461a0cb049c3
DominiekMeers <70231002+DominiekMeers@users.noreply.github.com>
2020-09-28 16:26:56 -0400

    New contact search with username
    Now only contacts show up that are linked with the user in the username

---
+++ LAMPAPI/ContactSearch2.php
@@ -0,0 +1,66 @@
+
+<?php
+
+//$name = $_GET['searchItem'];
+$inData = getRequestInfo();
+$userName = $inData["userName"]; 
+$firstName = $inData["firstName"];
+$lastName = $inData["lastName"];
+
+//$conn is used to connect to database
+$conn = new mysqli("localhost", "DomUserDB", "1209huis@.M", "UserDB");
+    if ($conn->connect_error) 
+    {
+        returnWithError( $conn->connect_error );
+    } 
+
+//goes through database to search for contacts with matching criteria in table
+$result = $conn->query("SELECT * FROM ContactTable WHERE UserName='" . "$userName" . "' AND FirstName LIKE '%" . "$firstName" . "%' AND LastName LIKE '%" . "$lastName" . "%'");
+
+//echo ("SELECT * FROM ContactTable WHERE UserName='" . "$userName" . "' AND FirstName LIKE '%{$firstName}%' AND LastName LIKE '%{$lastName}%'"."<BR>");
+
+$searchCount = $result->num_rows;
+
+$retValue = '{"userName":"' . $userName . '","contacts":';
+$retValue .= '[';
+while ($searchCount > 0)
+{
+    $row = mysqli_fetch_array($result);
+    //echo ($row['ContactID'] . " " . $row['FirstName'] . " " . $row['LastName']);
+        //echo "<br>";
+    $retValue .= '{"contactId":' . $row['ContactID'] . ',"firstName":"' . $row['FirstName'] . '","lastName":"' . $row['LastName'] . '"';
+    if ($searchCount != 1)
+    {
+        $retValue .= ' },';
+        //$searchResults .= ",";
+    }
+    else
+    {
+        $retValue .= ' }';
+    }
+    $searchCount--;
+}
+$result->free_result();
+    mysqli_close($conn);
+
+$retValue .= ']';
+$retValue .= '}';
+sendResultInfoAsJson( $retValue );
+
+function returnWithError( $err )
+{
+    $retValue = '{"userName":"","firstName":"","lastName":"","error":"' . $err . '"}';
+    sendResultInfoAsJson( $retValue );
+}
+
+function getRequestInfo()
+{
+    return json_decode(file_get_contents('php://input'), true);
+}
+
+function sendResultInfoAsJson( $obj )
+{
+    header('Content-type: application/json');
+    echo $obj;
+}
+?>
